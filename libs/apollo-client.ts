cc0ef403f08d681cadeb658bc8f28b544b3e54d1
kvarson <kvaratskheliadima@gmail.com>
2025-02-25 15:48:43 +0400

    setup apollo for subscription websockets.

--- libs/apollo-client.ts
+++ libs/apollo-client.ts
@@ -1,13 +1,37 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  split,
+  from,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import { setContext } from "@apollo/client/link/context";
+import { getMainDefinition } from "@apollo/client/utilities";
 import Cookies from "js-cookie";
-
 const httpLink = new HttpLink({
   uri: "https://take-home-be.onrender.com/api",
 });
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "wss://take-home-be.onrender.com/api",
+    connectionParams: () => ({
+      authToken: Cookies.get("token"),
+    }),
+    on: {
+      connected: () => console.log("WebSocket connected successfully"),
+      closed: () => console.log("WebSocket closed"),
+      error: (err) => console.error("WebSocket Error", err),
+    },
+  })
+);
+
 const authLink = setContext(async (_, { headers }) => {
-  const token = Cookies.get("token");
+  const token = Cookies.get("token") || "";
+  console.log("token,", token);
+
   return {
     headers: {
       ...headers,
@@ -16,8 +40,22 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
+const httpLinkWithAuth = from([authLink, httpLink]);
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    console.log("Definition:", definition);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLinkWithAuth
+);
+
 const client = new ApolloClient({
-  link: from([authLink, httpLink]),
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
