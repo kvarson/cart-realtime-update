bb8f8f058a8fc8116cd205c081d9d8fd043f3d4c
kvarson <kvaratskheliadima@gmail.com>
2025-02-25 20:08:07 +0400

    fix for reloading on products page was getting the unregistered so it was skipping request onGetCart, which resulted crash. now in useAuth we check if there is token it is authenticated.

--- src/app/stateManagement/AuthContext.tsx
+++ src/app/stateManagement/AuthContext.tsx
@@ -1,10 +1,17 @@
 import { AuthContextType, AuthProviderProps } from "@/types/interfaces";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isRegistered, setIsRegistered] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (Cookies.get("token")) {
+      setIsRegistered(true);
+    }
+  }, [isRegistered]);
 
   return (
     <AuthContext.Provider value={{ isRegistered, setIsRegistered }}>
